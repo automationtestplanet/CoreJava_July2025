a6a100d86b51e6bc78f4ccf8111660bdff35e790
automationtestplanet <145249195+automationtestplanet@users.noreply.github.com>
2025-07-25 10:29:21 +0530

    edits done in remote repo

--- src/fundamentals/InterviewQuestions.java
+++ src/fundamentals/InterviewQuestions.java
@@ -199,6 +199,23 @@ public class InterviewQuestions {
 		
 	}
 
+	public static void checkNumberPalindrome(int number) { // java
+		int revNum = 0;
+		int tempNum = number;
+		while(tempNum > 0) {
+			int remainder = tempNum % 10;  // 3, 2, 1
+			revNum = (revNum*10) + remainder;  // 0+3 = 3, 30+2 = 32, 320+1= 321
+			tempNum = tempNum / 10;  // 12, 1, 0
+		}		
+		
+		if(revNum == number) {
+			System.out.println(number+ " is a Palindorme");
+		}else {
+			System.out.println(number+ " is not a Palindorme");
+		}
+		
+	}
+
 	public static void main(String[] args) {
 //		swapVariables(100, 200);
 
