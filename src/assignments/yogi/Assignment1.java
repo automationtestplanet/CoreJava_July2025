27f461824bb717dceef1d59f57a6570fbe03f7b7
Yogesh <yogeshkilli759@gmail.com>
2025-07-29 05:46:43 +0530

    assignment1 by yogi

--- src/assignments/yogi/Assignment1.java
+++ src/assignments/yogi/Assignment1.java
@@ -1,5 +1,16 @@
 package assignments.yogi;
 
-public class Assignment1 {
-	//Write a reusable code to print even numbers form the given range
+public class Assignment01 {
+	public static void printEvennumbers(int startRange, int endRange) {
+		
+		for(int i = startRange; i <= endRange; i++) {
+			
+			if(i%2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		printEvennumbers(1, 100);
+	}
 }
